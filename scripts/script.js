38f5bace7fee72ffd1ecc75fcbe9fd941e322e44
Adam Więcek <adamw28@gmail.com>
2017-11-30 20:27:33 +0100

    Update project plus space

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,15 @@
 var x=prompt("Podaj wysokość choinki");
-function drawTree(x){
+function drawTree(x){var k=x;
 	for (var i=1; i<=x; i++){
 		var star='';
+		for(k;k>0;k--){
+			star+=' ';
+			}
 		for (var j=1; j<=i; j++){
 			star += '*';
 		}
-		console.log(star);
+	console.log(star);
+	k=x-i;
 	}
 }
 drawTree(x);
