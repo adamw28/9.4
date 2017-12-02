0ce69641d72e4515be0e2ca5228e61bbbd7b902c
Adam Więcek <adamw28@gmail.com>
2017-12-03 00:25:03 +0100

    Correct project

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,11 @@ var x=prompt("Podaj wysokość choinki");
 function drawTree(x){var k=x;
 	for (var i=1; i<=x; i++){
 		var star='';
-		for(k;k>0;k--){
+		for(k;k>1;k--){
 			star+=' ';
 			}
 		for (var j=1; j<=i; j++){
-			star += '*';
+			star += '**';
 		}
 	console.log(star);
 	k=x-i;
